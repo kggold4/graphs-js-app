398ba51dbb6eec5ec3956ba5c6259241be5a856e
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-08-21 20:27:55 +0300

    add queue data structure

--- js/api/queue.js
+++ js/api/queue.js
@@ -1,20 +1,9 @@
 // this class represents a queue data structure
-
-class Queue {
-    
-
+class Queue {   
     constructor() { this.elements = []; }
-
     enqueue(item) { this.elements.push(item); }
-
     dequeue() { return this.elements.shift(); }
-
     peek() { return !this.isEmpty() ? this.elements[0] : undefined; }
-
     size() { return this.elements.length; }
-
     isEmpty() { return this.size() == 0; }
-
-    
-
 }
