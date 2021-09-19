58c816b219195be18ad0849c8659ad7016a0651a
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-09-20 01:38:33 +0300

    improve contorller and connect to gui

--- js/controller.js
+++ js/controller.js
@@ -1,15 +1,58 @@
+// controller class is the class that controll the api and the gui as one in this project
+// this is the interface between the user and the data-startures/algorithms and the graphical user interface
 class Controller {
     constructor() {
         this.graph = new Graph();
+        this.graphAlgo = new GraphAlgo();
+
         this.prevGraphs = new Stack();
         this.nextGraphs = new Stack();
-        this.graphAlgo = new GraphAlgo();
+        
+        this.border = new Border();
     }
 
-    initAlgo() { this.graphAlgo.init(this.graph); }
+    /**
+     * GUI functions
+     */
 
+    refresh_graph() {
+        this.drawNodes();
+        this.drawEdges();
+    }
+
+    drawNodes() {
+        for(const node of this.graph.nodes) {
+            this.border.drawNode(node.id, node.position, node.info, node.tag);
+        }
+    }
+
+    drawEdges() {
+        for(const node_id of this.graph.getNodesList()) {
+            for(const child of this.graph.getChilds(node_id)) {
+                this.border.drawEdge(this.graph.getNode(node_id),
+                                     this.graph.getNode(child),
+                                     this.graph.getEdgeDistance(parseInt(node_id), parseInt(child)));
+            }
+        }
+        
+    }
+
+    /**
+     * Graph data structure functions
+     */
+
+    // initialize the graph to the graph algo
+    initAlgo() {
+        let g = this.graphAlgo.getGraph();
+        if(g == null || this.graph.getMc() != g.getMc()) {
+            this.graphAlgo.init(this.graph);
+        }
+    }
+
+    // each operation save the state current graph in the prev stack
     savePrev() { this.initAlgo(); this.prevGraphs.push(this.graphAlgo.copy()); }
 
+    // return to the last graph
     prev() {
         this.initAlgo();
         this.nextGraphs.push(this.graphAlgo.copy());
@@ -17,21 +60,29 @@ class Controller {
         if(prev_graph != null) this.graph = prev_graph;
     }
 
+    // return the the next graph (after doing prev)
     next() {
-        this.initAlgo();
-        this.
+        this.savePrev();
+        let next_graph = this.nextGraphs.pop();
+        if(next_graph != null) this.graph = next_graph;
     }
 
+    // return true the graph is contains a node with the id number
     hasNode(id) { return this.graph.hasNode(id); }
 
+    // adding a node by id number
     addNode(id, position, tag, info) { this.savePrev(); this.graph.addNode(id, position, tag, info); }
 
+    // remove a node by id number
     removeNode(id) { this.savePrev(); return this.graph.removeNode(id); }
 
+    // retrun true if id1 is connected by edge to id2 (id1 and id2 are id number of nodes in the graph)
     hasEdge(id1, id2) { return this.graph.addEdge(id1, id2); }
 
+    // connect between two nodes in the graph (id1 and id2 are id number of nodes in the graph)
     addEdge(id1, id2) { this.savePrev(); this.graph.addEdge(id1, id2); }
 
+    // remove connection between two nodes in the graph (id1 and id2 are id number of nodes in the graph)
     removeEdge(id1, id2) { this.savePrev(); this.graph.removeEdge(id1, id2); }
 
     // return number of nodes in the graph
@@ -48,4 +99,30 @@ class Controller {
 
     // return a list with all nodes id (integer) that the graph contains
     getNodesList() { return this.graph.getNodesList(); }
+
+    /**
+     * Graph algorithms functions
+     */
+
+    // return true if all the nodes in the graph are connected with edges using BFS algorithm
+    isConnected() {
+        this.initAlgo();
+        return this.graphAlgo.isConnected();
+    }
+
+    // return the shortest path between two nodes using Dijkstra algorithm
+    shortestPath(id1, id2) {
+        this.initAlgo();
+        return this.graphAlgo.shortestPath(id1, id2);
+    }
+
+    shortestPathDist(id1, id2) {
+        this.initAlgo();
+        return this.graphAlgo.shortestPathDist(id1, id2);
+    }
+
+    // return the distance of the graph diameter
+    diameterDistance() {
+        return this.graphAlgo.diameterDistance();
+    }
 }
